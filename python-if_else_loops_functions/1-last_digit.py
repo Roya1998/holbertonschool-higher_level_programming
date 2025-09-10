227f03147418c9f77375a4cc79324751b2ed00d5
Roya1998 <royayusifbayli@gmail.com>
2025-09-10 15:48:17 +0000

    negative number problem solve

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 string="Last digit of"
 lastNum=abs(number) % 10
-
+if number < 0:
+    lastNum = -lastNum
 if lastNum > 5:
     print(string, number, "is", lastNum, "and is greater than 5")
 elif lastNum == 0:
     print(string, number, "is", lastNum, "and is 0")
 elif  lastNum !=0 and lastNum < 6:
-    print(string,number, "is", lastNum, "and is less than 6 and is not 0")
+    if lastNum > 0:
+        print(string,number, "is", lastNum, "and is less than 6 and is not 0")
+    else:
+        print(string,number, "is", lastNum, "and is less than 6 and is not 0")
 else:
     print("\n")
