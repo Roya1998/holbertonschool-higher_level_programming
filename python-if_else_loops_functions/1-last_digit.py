0c7b0bf3778975fd148c11c9cef4781b25cf098f
Roya1998 <royayusifbayli@gmail.com>
2025-09-10 15:24:26 +0000

    newline remove from each line

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ string="Last digit of"
 lastNum=abs(number) % 10
 
 if lastNum > 5:
-    print(string, number, "is", lastNum, "and is greater than 5\n")
+    print(string, number, "is", lastNum, "and is greater than 5")
 elif lastNum == 0:
-    print(string, number, "is", lastNum, "and is 0\n")
+    print(string, number, "is", lastNum, "and is 0")
 elif  lastNum !=0 and lastNum < 6:
     print(string,number, "is", lastNum, "and is less than 6 and is not 0")
 else:
