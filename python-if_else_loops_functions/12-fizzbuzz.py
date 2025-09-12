022be8ee0fa686bfb49ca5b8c5a650c3842ebafd
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-13 02:05:10 +0400

    extra space remove

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for x in range(1, 101):
        if x % 3 == 0 and x % 5 == 0:
-           print("Fizz Buzz", end=" ")
+           print("Fizz Buzz")
        elif x % 3 == 0:
            print("Fizz", end=" ")
        elif x% 5 == 0:
