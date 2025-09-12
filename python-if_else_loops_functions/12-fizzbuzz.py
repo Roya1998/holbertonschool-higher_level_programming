188901cbb9641644b851f153e145912fed416577
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-13 02:02:38 +0400

    all numbers place in row position

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for x in range(1,101):
-        if x % 3 == 0 and x % 5 == 0:
-           print("Fizz Buzz")
-        elif x % 3 == 0:
-           print("Fizz")
-        elif x% 5 == 0:
-           print("Buzz")
-        else:
-           print(x)
+    for x in range(1, 101):
+       if x % 3 == 0 and x % 5 == 0:
+           print("Fizz Buzz", end=" ")
+       elif x % 3 == 0:
+           print("Fizz", end=" ")
+       elif x% 5 == 0:
+           print("Buzz", end=" ")
+       else:
+           print(x, end=" ")
