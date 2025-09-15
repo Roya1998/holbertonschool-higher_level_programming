6fffbd91cdbc44ed8b392976b122ca4cb8084282
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-15 20:25:29 +0400

    space problem solve

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import sys 
+import sys
 
 if __name__ == "__main__":
 
     def addition():
         result = 0
-        for x in range(1,len(sys.argv)):
+        for x in range(1, len(sys.argv)):
             result += int(sys.argv[x])
         print(result)
 
