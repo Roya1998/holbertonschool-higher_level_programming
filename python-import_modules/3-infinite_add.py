bc306ee31ab89e0592fe30d4a58f5c581e72ef3a
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-15 20:23:59 +0400

    4th task

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys 
+
+if __name__ == "__main__":
+
+    def addition():
+        result = 0
+        for x in range(1,len(sys.argv)):
+            result += int(sys.argv[x])
+        print(result)
+
+    addition()
