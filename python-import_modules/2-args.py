24012c1c7a544cb93533ac5fc8621e0d2be6684e
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-15 19:32:17 +0400

    indetation fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,17 +3,17 @@ import sys
 
 if __name__ == "__main__":
 
-  args = sys.argv[1:]
-  argv_length=len(args)
+    args = sys.argv[1:]
+    argv_length = len(args)
 
-  if argv_length == 0:
+    if argv_length == 0:
         print("{}.".format("0 arguments"))
-  elif argv_length == 1:
-       print("{}:".format("1 argument"))          
-  else:
+    elif argv_length == 1:
+        print("{}:".format("1 argument"))          
+    else:
         print("{} {}:".format(argv_length, "arguments"))
 
 
 
-  for x in range(argv_length): 
+    for x in range(argv_length): 
         print("{}: {}".format(x+1, args[x]))
