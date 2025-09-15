d1e50b97a5934056167911ab363430b0c45aece6
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-15 17:14:30 +0400

    string format add

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ import sys
 if __name__ == "__main__":
 
     if (len(sys.argv) == 1):
-        print(len(sys.argv), "argument")
+        print("{}: {}".format(len(sys.argv),"argument"))
     else:
-        print(len(sys.argv), "arguments")
+        print("{}: {}".format(len(sys.argv), "arguments"))
 
 
 
     for x in range(len(sys.argv)): 
-        print(x,":",sys.argv[x])
+        print("{}: {}".format(x,sys.argv[x]))
