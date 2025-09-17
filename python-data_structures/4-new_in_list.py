6bd02de1fb86d7b595b9362ebb1159204f2be25a
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-17 21:59:07 +0400

    5th task

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    internalList= my_list.copy()
-    if idx < 0 or idx >= len(my_list):  
-         return my_list
+    internalList = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return my_list
     else:
-         internalList[idx] = element 
-         return internalList
+        internalList[idx] = element 
+        return internalList
