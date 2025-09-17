ebb97a64d0fe25074d7aed5b1fc477ab7fc7c964
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-17 21:59:51 +0400

    white space removed

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -5,5 +5,5 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        internalList[idx] = element 
+        internalList[idx] = element
         return internalList
