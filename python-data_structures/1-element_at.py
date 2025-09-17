77d0ab6f67a8c4d8e2ba86d2c52c02af88519ee1
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-17 18:08:10 +0400

    deleting print

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,5 +4,6 @@ def element_at(my_list, idx):
 
         if idx < 0 or idx > len(my_list):
           return None
-        return my_list[idx]
-        print(num)
+        else: 
+           return my_list[idx]
+
