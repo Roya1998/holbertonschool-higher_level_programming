5af706261f248694525fb00caf4136e07dda8f9d
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-21 01:16:11 +0400

    5th task

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
 def only_diff_elements(set_1, set_2):
-    
-     differentElements = set_1 ^ set_2
-     return differentElements
- 
+    differentElements = set_1 ^ set_2
+    return differentElements
