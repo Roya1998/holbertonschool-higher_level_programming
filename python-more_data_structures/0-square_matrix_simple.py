13ff9bb9d419bd1a30f8f9e675588bd3d974af9d
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-20 23:55:19 +0400

    1st task

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    
+    squareMatrix=[]
+    
+    for num in matrix:
+        newRow=[]
+        for x in num:
+            square=x*x
+            newRow.append(square)
+            squareMatrix.append(newRow)
+    return squareMatrix
+    print("Num is", result)
---
+++ python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+This file is created for more data structures list and dictionary.
