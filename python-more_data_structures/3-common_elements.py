3e76fc312edf6e2f44d21873821549cd7977e6ce
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-21 01:05:19 +0400

    indentation problem solve

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2): 
-    
-      commonElements = set_1.intersection(set_2)
-      return commonElements
+def common_elements(set_1, set_2):
+
+    commonElements = set_1.intersection(set_2)
+    return commonElements
