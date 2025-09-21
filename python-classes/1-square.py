ba85b134c55a60b51bbc2e08ff05953e69ae5420
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-09-21 23:05:42 +0400

    2nd task

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class Square with a private size attribute."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize the square with a private size attribute."""
+        self.__size = size
