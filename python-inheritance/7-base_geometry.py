1a82d7c5a5d5c379096d58a87660b639a9dc21f2
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-04 18:11:30 +0400

    trailing white space solve

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,7 +13,7 @@ class BaseGeometry:
         """Validates that value is a positive integer
         Args:
             name: string name of the parameter
-            value: value to validate 
+            value: value to validate
         Raises:
             TypeError: if value is not an integer
             ValueError: if value is less or equal to 0
