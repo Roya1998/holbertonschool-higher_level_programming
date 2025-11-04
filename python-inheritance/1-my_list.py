68fe1de810f0f2579b4ebdbedfcf3b1bd152b13b
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-04 18:05:11 +0400

    2nd task added

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Defines MyList class that inherits from list."""
+"""Module that defines MyList class"""
+
 
 class MyList(list):
-    """Custom list class that inherits from list."""
+    """MyList class that inherits from list"""
 
     def print_sorted(self):
-        """Print the list sorted in ascending order."""
+        """Prints the list in ascending sorted order"""
         print(sorted(self))
