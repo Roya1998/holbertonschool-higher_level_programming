d10bbe3dfc5b32fcd871ef368980f70d806f04ef
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-18 19:50:05 +0400

    user name correct

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- This creates the user user_0d_1 with all privileges.
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+    IDENTIFIED BY 'user_0d_1';
 GRANT ALL PRIVILEGES
    ON *.*
    TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+   IDENTIFIED BY 'user_0d_1';
 FLUSH PRIVILEGES;
