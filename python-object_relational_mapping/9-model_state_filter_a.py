684d6d9792f049fb0ab1a075d89257f20e94f19f
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-19 18:02:02 +0400

    10th task

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects that contain the letter 'a' 
+Script that lists all State objects that contain the letter 'a'
 from the database hbtn_0e_6_usa, using SQLAlchemy.
 The script takes 3 arguments: mysql username, mysql password,database name.
 Results are sorted in ascending order by states.id.
