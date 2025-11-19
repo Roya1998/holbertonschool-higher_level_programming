a53108f2ca15bae77d03b7f423e20c232cf3d667
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-19 18:30:14 +0400

    11th task

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -8,20 +8,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) != 5:
-        print("Usage: ./10-model_state_my_get.py <username> <password> <database> <state_name>")
+    if len(sys.argv) != 5:
+        print("Usage: ./10-model_state_my_get.py <username> <password> "
+              "<database> <state_name>")
         sys.exit(1)
-    username, password, db_name, state_name = argv[1], argv[2], argv[3], argv[4]
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
-        pool_pre_ping=True
-    )
+    username, password, db_name, state_name = sys.argv[1], sys.argv[2], \
+                                                sys.argv[3], sys.argv[4]
+    # Create engine to connect to the MySQL database
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:'
+                           f'3306/{db_name}', pool_pre_ping=True)
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
+    # Query for the state by name
     state = session.query(State).filter(State.name == state_name).first()
+    # If state is found, print its id, else print "Not found"
     if state:
         print(state.id)
     else:
         print("Not found")
+    # Close session
     session.close()
