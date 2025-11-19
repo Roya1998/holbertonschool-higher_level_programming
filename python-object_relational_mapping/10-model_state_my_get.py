cb2098af5cc65158ab8586ad1bc92097bd45943f
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-19 18:21:45 +0400

    11th task

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
-"""
-Prints the State object with the name passed as argument from
-the database hbtn_0e_6_usa
-"""
+# Lists the State object with the name passed as argument
+# from the database hbtn_0e_6_usa.
+# Usage: ./10-model_state_my_get.py <mysql username> /
+#                                   <mysql password> /
+#                                   <database name>
+#                                   <state name searched>
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
+
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>"
-              .format(sys.argv[0]))
-        sys.exit(1)
-    username, password, database, state_name = sys.argv[1], sys.argv[2], \
-                                               sys.argv[3], sys.argv[4]
-    # Connect to the database
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(username,
-                                                          password,
-                                                          database),
-        pool_pre_ping=True
-    )
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    try:
-        # Query the state safely
-        state = session.query(State).filter(State.name == state_name).first()
-        if state:
-            print(state.id)
-        else:
-            print("Not found")
-    finally:
-        session.close()
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
+        print("Not found")
