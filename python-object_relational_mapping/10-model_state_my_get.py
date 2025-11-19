72ae4c8f9a702e2ac0ab397d8aba12f0423cd1b1
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-19 18:06:05 +0400

    11th task

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -9,12 +9,16 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
         sys.exit(1)
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username, password, database, state_name = sys.argv[1], sys.argv[2], \
+                                               sys.argv[3], sys.argv[4]
     # Connect to the database
     engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(username,
+                                                          password,
+                                                          database),
         pool_pre_ping=True
     )
     Session = sessionmaker(bind=engine)
