0cd8a47112d7127a464c63440d9a18394329771e
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-19 14:41:14 +0400

    empty space remove

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,12 +2,10 @@
 """Script that lists all states with name starting with N"""
 import MySQLdb
 import sys
-
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -15,14 +13,11 @@ if __name__ == "__main__":
         passwd=password,
         db=database
     )
-    
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
 ORDER BY id ASC")
     rows = cursor.fetchall()
-    
     for row in rows:
         print(row)
-    
     cursor.close()
     db.close()
