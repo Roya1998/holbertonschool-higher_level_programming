2b16d68e0a7771578fd33b823723f39f09d5d610
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-25 18:27:15 +0400

    11th task edit

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that adds the State object 'Louisiana'
-to the database hbtn_0e_6_usa.
+Adds the State object 'Louisiana' to the database hbtn_0e_6_usa.
 """
 
 import sys
@@ -11,29 +10,22 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./11-model_state_insert.py <username> <password> <database>")
-        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Connect to MySQL server
     engine = create_engine(
         f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
         pool_pre_ping=True
     )
 
-    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Create new State object
     new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
 
-    # Print the new state's id
     print(new_state.id)
 
-    # Close session
     session.close()
