007da430d5dc068857151b6b93f24b615b136773
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-25 19:31:47 +0400

    newline added to the file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -16,3 +16,11 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    
+    
+    
+    
+    
+    
+    
+    
