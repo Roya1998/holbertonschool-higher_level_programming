9688b139d3d9349a679bf0a6a9294dd9b3604753
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-11-25 18:24:16 +0400

    12th task

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python3
 """
-Script that prints the State object with the name passed as argument
-from the database hbtn_0e_6_usa.
+Script that adds the State object 'Louisiana'
+to the database hbtn_0e_6_usa.
 """
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: ./10-model_state_my_get.py <username> <password> "
-              "<database> <state_name>")
+    if len(sys.argv) != 4:
+        print("Usage: ./11-model_state_insert.py <username> <password> <database>")
         sys.exit(1)
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], \
-                                                sys.argv[3], sys.argv[4]
-    # Create engine to connect to the MySQL database
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:'
-                           f'3306/{db_name}', pool_pre_ping=True)
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Connect to MySQL server
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
+        pool_pre_ping=True
+    )
+
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query for the state by name
-    state = session.query(State).filter(State.name == state_name).first()
+    # Create new State object
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
 
-    # If state is found, print its id, else print "Not found"
-    if state:
-        print(state.id)
-    else:
-        print("Not found")
+    # Print the new state's id
+    print(new_state.id)
 
     # Close session
     session.close()
